ca5778dddf19915e1c595ea94434380f074cd78f
rohanphillips <rohan@solutions-insurance.com>
2020-11-14 16:21:38 -0600

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,31 @@
 // Code DigitalClicker Component Here
+import React, { Component } from 'react';
+
+export default class DigitalClicker extends Component {  
+  constructor(){
+    super()
+    this.state = {
+      timesClicked: 0
+    }
+  }
+
+  getTimesClicked = () => {
+    return this.state.timesClicked
+  }
+
+  updateTimesClicked = () => {
+    this.setState(previousState => { 
+      return {
+        timesClicked: previousState.timesClicked + 1 
+      }
+    })
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.updateTimesClicked}>{this.getTimesClicked()}</button>
+      </div>
+    )
+  }
+  
+}
